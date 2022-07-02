8d6ec7af283f84731d0598f1458ce3a3290366ef
Yomal <41592985+numbersletter@users.noreply.github.com>
2022-07-02 09:26:38 -0700

    Update go.js
    cleaned up

--- js/go.js
+++ js/go.js
@@ -10,28 +10,29 @@ document.addEventListener('DOMContentLoaded',function()
 }
 );
 
-function findStream()
+function decipherTextArea()
 {
-    var input = document.getElementById("links");
-    var text = input.innerText;
+    let input = document.getElementById("links");
+    let text = input.innerText;
     let sparsed_text = text.match(/https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}/igm);
     //sparsed text is a string ARRAY with links
+    return sparsed_text;
+}
 
-    //console.log(sparsed_text);
-    localStorage.setItem("LINKS",JSON.stringify(sparsed_text));
+function findStream()
+{
+    let SPARSED = decipherTextArea();
+    localStorage.setItem("LINKS",JSON.stringify(SPARSED));
 
-    let openlink = sparsed_text[Math.floor(Math.random() * (sparsed_text.length))];
+    let openlink = SPARSED[Math.floor(Math.random() * (sparsed_text.length))];
     window.open(openlink,"_blank");
     //console.log(localStorage.getItem('LINKS'))
 
 }
 
 function saveLinks()
-{   var input = document.getElementById("links");
-    var text = input.innerText;
-    let sparsed_text = text.match(/https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}/igm);
-
-    localStorage.setItem('LINKS', JSON.stringify(sparsed_text));
+{   
+    localStorage.setItem('LINKS', JSON.stringify(decipherTextArea()));
     //console.log(localStorage.getItem("LINKS"));
 }
 
